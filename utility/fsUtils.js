b78a85a55b9f11792bd61daaa5ff761e356a846a
Khanh Truong <khanhtruong_1992@yahoo.com>
2021-12-08 17:20:17 -0800

    added ulility

--- utility/fsUtils.js
+++ utility/fsUtils.js
@@ -1,5 +1,28 @@
 const fs = require( 'fs' );
 const util = require( 'util' );
 
-const readFromFile = util.promisify( fs.readFile );
+const readFile = util.promisify( fs.readFile );
 
+const writeToFile = ( filePath, content ) =>
+	fs.writeFile( filePath, content, ( err ) =>
+		err ? console.log( err ) : console.log( `path to written file: ${filePath}` )
+	);
+
+
+const appendToFile = ( obj, filePath ) => {
+        readFile( filePath )
+            .then( ( data ) => {
+                const parsedData = JSON.parse( data );
+    
+                parsedData.push( obj );
+    
+                const dataJSON = JSON.stringify( parsedData, null, 4 );
+    
+                writeToFile( filePath, dataJSON );
+            } );
+};
+module.exports = {
+	readFile,
+	writeToFile,
+	appendToFile
+};
